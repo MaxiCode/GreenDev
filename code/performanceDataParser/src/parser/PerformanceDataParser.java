907f37f27a2bf2c71d507fb601d4300cbe4a5565
Max Weber <max.weber@uni-weimar.de>
2017-12-11 14:36:50 +0100

    deleate eclipse stuff, added performance parser multifile analysis

--- code/performanceDataParser/src/parser/PerformanceDataParser.java
+++ code/performanceDataParser/src/parser/PerformanceDataParser.java
@@ -1,35 +1,59 @@
 package parser;
 
+import java.io.File;
+
 public class PerformanceDataParser {
 
 	private static String performanceOutputFolder = 
 			"/home/max/uni/GreenDev/code/subject"
 			+ "Systems/h2database/h2/";
-	private static String path = 
+	private static String pathToProfile = 
 			"/home/max/uni/GreenDev/code/subject"
-			+ "Systems/h2database/h2/profile.txt";
-	private static String profileFile = 
-			"profile.txt";
+			+ "Systems/h2database/h2/profile1.txt";
+	private static String profileFileName = 
+			"profile";
+	private static String profileFileExtension =
+			".txt";
+	
+//	private Map<Integer, PerformanceDataModel> models = new HashMap<Integer, PerformanceDataModel>();
+	
+	
+	private static boolean parsingFolder = false;
+	
 	
 	public static void main (String[] args) {
 		System.out.println("Start parsing performance data.");
-//		
-//		if 
 		
-		PerformanceDataModel model = new PerformanceDataModel();
-		model.parseFile(path);
 		
-//		model.printData();
-		model.printPerformanceFractionPerFunction();
+		if (parsingFolder) {
+			File folder = new File(performanceOutputFolder);
+			System.out.println("profile Folder exists: " + (folder.exists() && folder.isDirectory()));
+			
+			File[] listOfFiles = folder.listFiles();
+			
+			for (File file : listOfFiles) {
+				if (file.isFile() && file.getName().contains(profileFileName) && 
+						file.getName().contains(profileFileExtension)) {
+					
+					System.out.println(file.getName());
+					PerformanceDataModel model = new PerformanceDataModel();
+					model.parseFile(file.getAbsolutePath());
+					model.printPerformanceFractionPerFunction();
+				}
+			}
+			
+		} else {
+		
+			File file = new File(pathToProfile);
+			System.out.println("profile exists: " + (file.exists() && file.isFile()));
+			
+			PerformanceDataModel model = new PerformanceDataModel();
+			model.parseFile(pathToProfile);
+			
+//			model.printData();
+			model.printPerformanceFractionPerFunction();
+		}
 		
 		System.out.println("Finished parsing performance data.");
-	}
-	
-	private static void runAllProfiles (String path, String file) {
-		
-	}
-	
-	private static void runOneProfile (String file) {
-		
 	}
 }
